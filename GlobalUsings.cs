3b4a50ee230b62c221a2294fba496f386f41dbc1
MikeNakis <git@michael.gr>
2025-10-24 22:55:21 +0200

    Added digital-garden-programming, made lots of changes to accommodate the move

---
+++ GlobalUsings.cs
@@ -0,0 +1,36 @@
+#pragma warning disable IDE0005 // Using directive is unnecessary.
+
+global using System.Collections.Generic;
+global using System.Linq;
+global using static System.Xml.XPath.Extensions;
+global using ConcurrentCollections = System.Collections.Concurrent;
+global using LegacyCollections = System.Collections;
+global using LinqExpr = System.Linq.Expressions;
+global using Math = System.Math;
+global using RegEx = System.Text.RegularExpressions;
+global using SpecializedCollections = System.Collections.Specialized;
+global using Sys = System;
+global using SysCodeAnalysis = System.Diagnostics.CodeAnalysis;
+global using SysCompiler = System.Runtime.CompilerServices;
+global using SysCompModel = System.ComponentModel;
+global using SysCompress = System.IO.Compression;
+global using SysConfig = System.Configuration;
+global using SysContracts = System.Diagnostics.Contracts;
+global using SysCrypto = System.Security.Cryptography;
+global using SysDiag = System.Diagnostics;
+global using SysDraw = System.Drawing;
+global using SysGlob = System.Globalization;
+global using SysInterop = System.Runtime.InteropServices;
+global using SysIo = System.IO;
+global using SysIoPipes = System.IO.Pipes;
+global using SysNet = System.Net;
+global using SysNum = System.Numerics;
+global using SysObjModel = System.Collections.ObjectModel;
+global using SysReflect = System.Reflection;
+global using SysTask = System.Threading.Tasks;
+global using SysText = System.Text;
+global using SysThread = System.Threading;
+global using SysXml = System.Xml;
+global using SysXmlLinq = System.Xml.Linq;
+
+#pragma warning restore IDE0005 // Using directive is unnecessary.
